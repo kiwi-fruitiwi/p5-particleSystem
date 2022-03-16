993327b1bfc34173cdc8de2411dad57b331b9884
kiwi-fruitiwi <zane.tian@gmail.com>
2022-03-16 11:35:34 -0700

    add fading particles. add noCursor +custom cursor

--- particle.js
+++ particle.js
@@ -1,13 +1,20 @@
 class Particle {
     constructor(x, y) {
-        this.pos = new p5.Vector(random(width), random(height))
+        this.pos = new p5.Vector(x, y)
         this.vel = p5.Vector.random2D()
         this.acc = new p5.Vector()
         this.r = random(2, 5)
-        this.target = new p5.Vector(x, y)
         this.maxspeed = 5
         this.maxforce = 1
         this.hue = random(360)
+
+        this.lifetime = random(50, 100)
+        this.img = loadImage('data/64.png')
+    }
+
+
+    finished() {
+        return this.lifetime < 0
     }
 
 
@@ -44,64 +51,6 @@ class Particle {
     }
 
 
-    fleeFromMouse() {
-        /* applies a fleeing behavior based on distance from mouse */
-        let mouse = new p5.Vector(mouseX, mouseY)
-        let flee = this.flee(mouse).mult(2)
-        let distance = p5.Vector.sub(this.pos, mouse).mag()
-        if (distance < 70) {
-            this.applyForce(flee)
-        }
-    }
-
-
-    /** applies arrival behavior, sending this vehicle to its 'home' */
-    returnToTextOrigin() {
-        let arrive = this.arrive(this.target)
-        this.applyForce(arrive)
-    }
-
-
-    seek(target) {
-        // this gives you a vector pointing from us to the target
-        let desired = p5.Vector.sub(target, this.pos)
-        desired.setMag(this.maxspeed)
-
-        // steering = desired - current
-        let steer = p5.Vector.sub(desired, this.vel)
-        return steer.limit(this.maxforce)
-    }
-
-
-    flee(target) {
-        return this.seek(target).mult(-1)
-        // watch out! technically, we should probably multiply our
-        // desired velocity by -1 first before limiting
-    }
-
-
-    // like seek, but we slow down as we approach our target :3
-    arrive(target) {
-        // this gives you a vector pointing from us to the target
-        let desired = p5.Vector.sub(target, this.pos)
-
-        // the distance between two points is the magnitude of the
-        // vector from one to the other
-        let distance = desired.mag()
-
-        let speed = this.maxspeed
-        if (distance < 100) {
-            speed = map(distance, 0, 100, 0, this.maxspeed)
-        }
-
-        desired.setMag(speed)
-
-        // steering = desired - current
-        let steer = p5.Vector.sub(desired, this.vel)
-        return steer.limit(this.maxforce)
-    }
-
-
     applyForce(f) {
         // F=ma, but we assume m=1, so F=a
         this.acc.add(f)
@@ -115,13 +64,16 @@ class Particle {
         this.vel.limit(this.maxspeed)
         this.vel.mult(0.995) // friction
         this.acc.mult(0)
+
+        this.lifetime -= random(0.5)
     }
 
 
     show() {
-        stroke(this.hue, 100, 100, 100)
-        fill(this.hue, 100, 100, 64)
+        stroke(this.hue, 100, 100, this.lifetime)
+        fill(this.hue, 100, 100, this.lifetime)
 
+        /* TODO add .png texture later */
         circle(this.pos.x, this.pos.y, this.r*2)
         fill(0, 0, 100)
     }
--- sketch.js
+++ sketch.js
@@ -4,17 +4,63 @@
  *
  *  ☐ wand with particle emitter for cursor
  */
+
+
 let font
+let instructions  /* div for instructions */
+
+
+let particles
+
 
 function preload() {
     font = loadFont('data/meiryo.ttf')
 }
 
+
 function setup() {
-    createCanvas(640, 360)
+    let cnv = createCanvas(600, 300)
+    noCursor()
+    cnv.parent('#canvas')
+
     colorMode(HSB, 360, 100, 100, 100)
+
+    /* initialize instruction div */
+    instructions = select('#ins')
+    instructions.html(`<pre>
+        [1,2,3,4,5] → no function
+        z → freeze sketch</pre>`)
+
+    particles = []
+    for (let i=0; i<100; i++) {
+        particles.push(new Particle(random(width), random(height)))
+    }
 }
+
 
 function draw() {    
     background(234, 34, 24)
+    fill(0, 0, 100, 70)
+    stroke(0, 0, 100)
+    circle(mouseX, mouseY, 10)
+
+    for (p of particles) {
+        if (!p.finished()) {
+            p.show()
+            p.update()
+            p.edges()
+        } else {
+
+        }
+    }
+}
+
+
+function keyPressed() {
+    /* stop sketch */
+    if (key === 'z') {
+        noLoop()
+        instructions.html(`<pre>
+            sketch stopped</pre>`)
+    }
 }
